d150368ca6e765d44287b3f6590f264773ffc3a9
Kaleb Klein <klein.jae@gmail.com>
2019-07-13 17:20:14 -0400

    arch agnostic folder naming
    Signed-off-by: Kaleb Klein <klein.jae@gmail.com>

--- build.bat
+++ build.bat
@@ -28,6 +28,12 @@ if %ARCH% EQU 32 (
     set VCR=vcredist_msvc2017_x64.exe
 )
 
+:: Setting system paths with needed toolchain locations
+set PF=%PROGRAMFILES(X86)%
+set CPATH=%PATH%
+set PATH=C:\Qt\%QT%\%MSVC%\bin;C:\Qt\Tools\QtCreator\bin;^
+%PF%\Microsoft Visual Studio\%VS%\Community\VC\Auxiliary\Build;%CPATH%;
+
 :: Command flags
 :: -b goes right to build
 :: -c goes right to clean
@@ -42,16 +48,10 @@ for %%A in (%*) do (
     )
 )
 
-:: Setting system paths with needed toolchain locations
-set PF=%PROGRAMFILES(X86)%
-set CPATH=%PATH%
-set PATH=C:\Qt\%QT%\%MSVC%\bin;C:\Qt\Tools\QtCreator\bin;^
-%PF%\Microsoft Visual Studio\%VS%\Community\VC\Auxiliary\Build;%CPATH%;
-
+:build
+echo.
 :: Setup C Build Environment
 call vcvarsall %VCA%
-
-:build
 echo.
 :: Gets max processor threads to compile on
 set /p J="Would you like to use all CPU cores? [Y/n] "
@@ -69,34 +69,33 @@ echo Building makefile
 qmake kpf-qt.pro -o staging\Makefile
 cd staging
 jom /F Makefile.release %J%
-copy /Y "%CWD%\staging\release\KPF.exe" "%CWD%\bin\win64\KPF.exe"
-copy /Y C:\Qt\vcredist\%VCR% "%CWD%\bin\win64\redist\%VCR%"
+copy /Y "%CWD%\staging\release\KPF.exe" "%CWD%\bin\windows\KPF.exe"
+copy /Y C:\Qt\vcredist\%VCR% "%CWD%\bin\windows\redist\%VCR%"
 echo.
 echo Copying required libraries
-copy /Y C:\Qt\%QT%\%MSVC%\bin\Qt5Core.dll "%CWD%\bin\win64\Qt5Core.dll"
-copy /Y C:\Qt\%QT%\%MSVC%\bin\Qt5Gui.dll "%CWD%\bin\win64\Qt5Gui.dll"
-copy /Y C:\Qt\%QT%\%MSVC%\bin\Qt5Widgets.dll "%CWD%\bin\win64\Qt5Widgets.dll"
-copy /Y C:\Qt\%QT%\%MSVC%\plugins\platforms\qwindows.dll "%CWD%\bin\win64\platforms\qwindows.dll"
+copy /Y C:\Qt\%QT%\%MSVC%\bin\Qt5Core.dll "%CWD%\bin\windows\Qt5Core.dll"
+copy /Y C:\Qt\%QT%\%MSVC%\bin\Qt5Gui.dll "%CWD%\bin\windows\Qt5Gui.dll"
+copy /Y C:\Qt\%QT%\%MSVC%\bin\Qt5Widgets.dll "%CWD%\bin\windows\Qt5Widgets.dll"
+copy /Y C:\Qt\%QT%\%MSVC%\plugins\platforms\qwindows.dll "%CWD%\bin\windows\platforms\qwindows.dll"
 echo.
 cd %CWD%
-echo Build Complete!
+echo KPF Build Complete!
 goto end
 
 :clean
 echo.
 echo Cleaning up binaries
-cd %CWD%\bin\win64
+cd %CWD%\bin\windows
 if exist KPF.exe del KPF.exe
 if exist Qt5Core.dll del Qt5Core.dll
 if exist Qt5Gui.dll del Qt5Gui.dll
 if exist Qt5Widgets.dll del Qt5Widgets.dll
 if exist platforms\qwindows.dll del platforms\qwindows.dll
 if exist redist\%VCR% del redist\%VCR%
-if exist kse.ini del kse.ini
 if exist logs rmdir /S /Q logs
 cd %CWD%
 if exist staging rmdir /S /Q staging
-echo Cleaning complete
+echo KPF Cleaning complete
 goto end
 
 :help
@@ -118,4 +117,6 @@ set VCR=
 set VCA=
 set PF=
 set ARCH=
-pause
+if not defined SKIP_PAUSE (
+    pause
+)
--- clean.bat
+++ clean.bat
@@ -1,2 +1,2 @@
 @echo off
-build.bat -c
+call build.bat -c
