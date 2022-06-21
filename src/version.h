1b70877f8da3f4aff4ebafa285164918609994a9
Kaleb Klein <apollyon156@protonmail.com>
2022-06-20 21:34:06 -0400

    Change version strings

--- src/version.h
+++ src/version.h
@@ -1,16 +1,16 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             1,1,0,0
-#define VER_FILEVERSION_STR         "1.1\0"
+#define VER_FILEVERSION             1,2,0,0
+#define VER_FILEVERSION_STR         "1.2\0"
 
-#define VER_PRODUCTVERSION          1,0,0,0
-#define VER_PRODUCTVERSION_STR      "1.1\0"
+#define VER_PRODUCTVERSION          1,2,0,0
+#define VER_PRODUCTVERSION_STR      "1.2\0"
 
 #define VER_COMPANYNAME_STR         "KSE Team"
 #define VER_FILEDESCRIPTION_STR     "KSE Path Finder"
 #define VER_INTERNALNAME_STR        "KPF"
-#define VER_LEGALCOPYRIGHT_STR      "Copyright (c) 2015-2019 KSE Team"
+#define VER_LEGALCOPYRIGHT_STR      "Copyright (c) 2015-2022 KSE Team"
 #define VER_LEGALTRADEMARKS1_STR    "All Rights Reserved"
 #define VER_LEGALTRADEMARKS2_STR    VER_LEGALTRADEMARKS1_STR
 #define VER_ORIGINALFILENAME_STR    "kpf-qt.exe"
