5fedc4dc935c97ceff0ace59f42bf957503eeda8
WessellUrdata <86524864+WessellUrdata@users.noreply.github.com>
2023-01-03 19:50:16 +0800

    Append "-linux" to version

--- src/version.h
+++ src/version.h
@@ -2,10 +2,10 @@
 #define VERSION_H
 
 #define VER_FILEVERSION             1,2,0,0
-#define VER_FILEVERSION_STR         "1.2\0"
+#define VER_FILEVERSION_STR         "1.2-linux\0"
 
 #define VER_PRODUCTVERSION          1,2,0,0
-#define VER_PRODUCTVERSION_STR      "1.2\0"
+#define VER_PRODUCTVERSION_STR      "1.2-linux\0"
 
 #define VER_COMPANYNAME_STR         "KSE Team"
 #define VER_FILEDESCRIPTION_STR     "KSE Path Finder"
